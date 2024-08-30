9dc0dfb3210a1e3347c302861e8147f11b4572b5
KuR0zZ <ferdinandiphone6@gmail.com>
2024-08-30 11:19:04 +0700

    Use pygame to create a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,12 +9,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable =  pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(X, Y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        for thing in drawable:
+            thing.draw(screen)
+
+        for thing in updatable:
+            thing.update(dt)
+
         pygame.display.flip()
 
         # limit framerate to 60
